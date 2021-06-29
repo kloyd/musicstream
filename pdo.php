4ddb08e514d714104910d1b7903ceba84138464c
kloyd <kelly.loyd@icloud.com>
2021-06-29 16:17:06 -0500

    change db user

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=songs',
-   'kelly', 'LemonCurry45');
+   'stream', 'pickles');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
